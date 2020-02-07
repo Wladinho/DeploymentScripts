0377e6b10501333509eb8f1d5001362ffd65e609
Vladimir Stefanović <stefanovic.vladimir@hotmail.com>
2020-02-07 21:11:29 +0100

    Update

---
+++ NewForest.ps1
@@ -0,0 +1,30 @@
+﻿    [CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)][string]$DomainName,
+    [Parameter(Mandatory=$true)][string]$DomainNetBiosName,
+    [Parameter(Mandatory=$true)][string]$DSRMPassword
+)
+
+# Install AD DS feature
+Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+
+# Define variables for the new forest 
+$DomainParameters = @{
+
+    DomainName                    = $DomainName
+    CreateDnsDelegation           = $false
+    ForestMode                    = "WinThreshold"
+    DomainNetbiosName             = $DomainNetBiosName
+    InstallDns                    = $true
+    DatabasePath                  = "C:\Windows\NTDS"
+    LogPath                       = "C:\Windows\NTDS"
+    SysvolPath                    = "C:\Windows\SYSVOL"
+    NoRebootOnCompletion          = $false
+    Force                         = $true
+    SafeModeAdministratorPassword = $DSRMPassword
+
+}
+
+# Install new forest
+Import-Module ADDSDeployment
+Install-ADDSForest @DomainParameters
