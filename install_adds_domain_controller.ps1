d43c9740c895366677b5c5d5643e1c5e66ec22e4
Vladimir Stefanović <stefanovic.vladimir@hotmail.com>
2020-03-19 20:02:39 +0100

    install_adds_domain_controller.ps1 - v1.0 / 2020-03-19

--- install_adds_domain_controller.ps1
+++ install_adds_domain_controller.ps1
@@ -1,28 +1,38 @@
 ﻿<#
-.Synopsis
+.SYNOPSIS
     Parameterized script for installing additional Domain Controller into existing AD DS forest.
      
 .DESCRIPTION
     Custom script with parameters for installing additional Domain Controller into existing AD DS forest.
-    Script will install all needed prerequisites, before promote destination server to Domain Controller. 
+    The script will install all needed prerequisites before promoting the destination server to Domain Controller. 
 
 .EXAMPLE
-    InstallAdditionalDomainController.ps1 -DomainName <> -DSRMPassword <> -DomainAdminPassword <> -DomainAdminUsername <>
+    install_adds_domain_controller.ps1 -DomainName "Contoso.com" -DSRMPassword "Pa$$w0rd1234" -DomainAdminPassword "Pa$$w0rd" -DomainAdminUsername "contoso\administrator"
+
+    .NOTES
+    File Name : install_adds_domain_controller.ps1
+    Version   : 1.0
+    Author    : Vladimir Stefanovic <vladimir@superadmins.com> 
+    Requires  : PowerShell
+
+    --- CHANGELOG ---
+    
+    v1.0 - 2020-03-18 / Initial version
 #>
 
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess=$True)]
 param (
     [Parameter(Mandatory=$true)][string]$DomainName,
-    [Parameter(Mandatory=$true)][string]$DSRMPassword,
+    [Parameter(Mandatory=$true)][String]$DSRMPassword,
     [Parameter(Mandatory=$true)][string]$DomainAdminUsername,
-    [Parameter(Mandatory=$true)][string]$DomainAdminPassword
+    [Parameter(Mandatory=$true)][String]$DomainAdminPassword
 )
 
 # Install AD DS feature
 Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
 
-# Define variables for the new forest
-$Password = ConvertTo-SecureString -String $DomainAdminPassword -AsPlainText -Force
+# Define variables and parameters for the new a domain controller
+$Password    = ConvertTo-SecureString -String $DomainAdminPassword -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential $DomainAdminUsername, $Password
 
 $DomainParameters = @{
